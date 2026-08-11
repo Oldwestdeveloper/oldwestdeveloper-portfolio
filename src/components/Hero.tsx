e21b89e28cf1365406bc67f36bc1e1d9d86c68a1
Guilherme Araujo <gui.dev28@outlook.com>
2026-08-11 16:36:49 -0300

    fix: CV

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -54,7 +54,7 @@ export default function Hero() {
             Contato
           </Link>
           <a 
-            href="https://drive.google.com/file/d/1Xg-RsATF8A5DR2XjLZbBeTf3RK9w3f4d/view?usp=sharing" 
+            href="https://drive.google.com/file/d/1rSkVytr52GF9uJVzb4V37st77UAHKDdO/view?usp=sharing" 
             target="_blank"
             rel="noopener noreferrer"
             className="border border-gray-400 text-gray-400 font-bold px-8 py-3 rounded-sm hover:border-white hover:text-white transition-all flex items-center justify-center gap-2"
